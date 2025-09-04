c412d94443280eeb2f208c1e61abb2be41530cfa
Kovu349 <103192973+Kovu349@users.noreply.github.com>
2025-09-04 00:45:45 +0000

    updates

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print("Welcome to the food ordering Chatbot")
 name = input("What is your name? ")
+age = int(input("Hello " + name + ", how old are you? "))
+
+if age >= 18:
+    print("You qualify for the full menu! ")
+elif age <= 18:
+    print("You qualify for the kid's menu! ")
