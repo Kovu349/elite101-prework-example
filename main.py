7f662c5039a352ee6c12d060ceef2b4c36aaab60
chidiebube08 <103192973+Kovu349@users.noreply.github.com>
2025-09-06 10:58:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ name = input("What is your name? ")
 age = int(input("Hello " + name + ", how old are you? "))
 print(str(age) + ". Nice! Now, how can I be of your assistance today? ")
 
-print("1. View the Menu.")
-print("2. Place an order")
-print("3. Track your order")
-print("4. Exit ")
+while True:
+    print("1. View the Menu.")
+    print("2. Place an order")
+    print("3. Track your order")
+    print("4. Exit ")
 
-choice = input("Please enter the number of your choice. ")
-
-if choice == "1":
-    print("We have Hamburgers, Tacos, and French Fries.")
-elif choice == "2":
-    print("Let's place an order.")
-elif choice == "3":
-    print("Let's track your order.")
-elif choice == "4":
-    print("Goodbye!")
-else:
-    print("Sorry. That's not an option, try again!")
+    choice = input("Please enter the number of your choice. ")
+    
+    if choice == "1":
+        print("We have Hamburgers, Tacos, and French Fries.")
+    elif choice == "2":
+        print("Let's place an order.")
+    elif choice == "3":
+        print("Let's track your order.")
+    elif choice == "4":
+        print("Goodbye!")
+        break
+    else:
+        print("Sorry. That's not an option, try again!")
